84368124495b8b973301d410ffb81d67967f343d
Lars Schneider <larsxschneider@gmail.com>
2012-12-29 22:09:03 +0100

    Update assets.

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
 
 pushd "$BASE_DIR"
 	rm bundle.zip
-	zip -r bundle background.js contentscript.js icon-128.png icon-48.png manifest.json 3rdParty/jQuery/jquery.min.js 3rdParty/ctags.js/ctags.js
+	zip -r bundle background.js contentscript.js Assets/icon-128.png Assets/icon-48.png manifest.json 3rdParty/jQuery/jquery.min.js 3rdParty/ctags.js/ctags.js
 popd
